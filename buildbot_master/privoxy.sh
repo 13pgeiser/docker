f17e6c5f80db31bfccf14314cd9811adb4bce271
Pascal Geiser <pgeiser@pgeiser.com>
2021-10-06 21:41:38 +0200

    Update the IP address only when it's not already there

--- buildbot_master/privoxy.sh
+++ buildbot_master/privoxy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 IP=$(/sbin/ifconfig eth0 | grep 'inet ' | tr -s ' ' | cut -d" " -f3)
-echo "listen-address  $IP:8118" >>/etc/privoxy/config
+if ! grep -q "listen-address  $IP:8118" /etc/privoxy/config
+then
+        echo "listen-address  $IP:8118" >>/etc/privoxy/config
+fi
 privoxy --no-daemon /etc/privoxy/config
