c88c9e3e20b4218ffe533a6a8bdbe2f9e943b23f
Pascal Geiser <10461883+13pgeiser@users.noreply.github.com>
2026-01-03 15:02:44 +0100

    Add local server to bind service

--- squid/start.sh
+++ squid/start.sh
@@ -3,6 +3,7 @@ set -ex
 echo "Updating list of blocked domains"
 curl -sS -L --compressed "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=nohtml&showintro=0&mimetype=plaintext" > /etc/squid/ad_block.txt
 echo "Starting bind9"
+echo "192.168.0.222 debiand843e635fb7" >>/etc/hosts
 /etc/init.d/named start
 echo "Starting squid..."
 /usr/sbin/squid -z
